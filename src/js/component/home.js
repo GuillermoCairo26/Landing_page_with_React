840a1cfd13d5e8b6b7cc9a24e8d512f7b3eece3d
Martin Luaces <martinluaces17@gmail.com>
2021-04-28 18:46:27 +0000

    colaborativo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Jumbotron } from "./jumbotron.js";
 import { Cards } from "./cards.js";
+import { NavBar } from "./navbar.js";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<Jumbotron />
-            <Cards/>
-		</div>
-	);
+    return (
+        <div className="text-center mt-5">
+            <Jumbotron />
+            <Cards />
+            <NavBar />
+
+        </div>
+    );
 }
